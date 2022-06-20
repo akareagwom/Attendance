ff2d1bd9959af3414fe9977315a251f65a9353c4
akareagwom <103905242+akareagwom@users.noreply.github.com>
2022-06-20 08:37:27 -0700

    20th.....
    20th...

---
+++ pages/Dashboard/Dashboard.js
@@ -0,0 +1,72 @@
+import { Calendar } from "react-calendar";
+import SideBar from "../../sidebar/SideBar";
+import TopBar from "../../topbar/TopBar";
+import 'react-calendar/dist/Calendar.css';
+import {useState} from "react"
+import BarChart from "../barchart/BarChart";
+import {UserData} from '../data/Data';
+import './Dashboard.css'
+
+
+const Dashboard = () => {
+    const [value, onChange] = useState(new Date());
+    const [newData,setNewData] = useState({
+        labels: UserData.map((data)=>data.month),
+        datasets: [
+            {
+                label: "Overall Attendance View",
+                data: UserData.map((data)=> data.UserGain),
+                barPercentage: 0.5,
+                backgroundColor: ["aqua","blue","aqua","grey","pink"],
+                barThickness: 10,
+                maxBarThickness: 8,
+                minBarLength: 2,
+               
+            },
+        ],
+    });
+
+
+    return ( 
+        <div >
+            <SideBar />
+            <TopBar />
+            <div className="see">
+             <BarChart chartData={newData}/>
+            </div >
+            <div className="cctv">
+              <Calendar value={value} onChange={onChange} />
+            </div>
+            <div className="mee">
+                <table className="leah">
+                <tr>
+                      <th>Name</th>
+                      <th>ID</th>
+                      <th>School</th>
+                      <th>Age</th>
+                      <th>Gender</th>
+                      <th>Email</th>
+                    </tr>
+                    <tr>
+                        <td> <img src="./avatar.png" alt="" srcset=""/> Weng Du</td>
+                        <td>2345623569</td>
+                        <td>FUT</td>
+                        <td>26</td>
+                        <td>Male</td>
+                        <td>wengdu@gmail.com</td>
+                      </tr>
+                      <tr>
+                        <td> <img src="./avatar.png" alt="" srcset="" /> Chun Li</td>
+                        <td>1234567891</td>
+                        <td>UJ</td>
+                        <td>20</td>
+                        <td>Female</td>
+                        <td>chunli@gmail.com</td>
+                      </tr>
+                </table>
+            </div>
+        </div>
+     );
+}
+ 
+export default Dashboard;
